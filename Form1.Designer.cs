1578aaa67d6ce6f3c792b6ceb2914a9efcecf585
johott <john.ottosson@miun.se>
2015-08-24 11:46:50 +0200

    johott size button

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -48,7 +48,7 @@
             this.button1.Location = new System.Drawing.Point(42, 141);
             this.button1.Margin = new System.Windows.Forms.Padding(2);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(78, 44);
+            this.button1.Size = new System.Drawing.Size(24, 23);
             this.button1.TabIndex = 1;
             this.button1.Text = "Finare  knapp";
             this.button1.UseVisualStyleBackColor = true;
