9a63bbcf9e677990fc11e21d409452da9e6e519d
johott <john.ottosson@miun.se>
2015-08-25 11:13:07 +0200

    sql utan db namn

--- Form1.cs
+++ Form1.cs
@@ -7,7 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using System.Data.SqlClient;
 namespace Nu
 {
     public partial class Form1 : Form
@@ -22,6 +22,16 @@ namespace Nu
             MessageBox.Show("develop branch. Erik lägger till text");
             MessageBox.Show("develop branch. john lägger till text");
             MessageBox.Show("Erik");
+
+
+            using (SqlConnection conn = new SqlConnection())
+            {
+                conn.ConnectionString = "Server=weblabb.miun.se;Port5432;Database=xxx;UserId=pgmvaru_g4;Password=trapets:SSL=true";
+               
+            }
+            
+            
+    
         }
 
         private void label2_Click(object sender, EventArgs e)
